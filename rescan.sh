4c01eaef64546ba9913df40351d734d869658626
Artur <arthur.escada.20@gmail.com>
2023-01-31 19:50:19 +0200

    Update rescan.sh

--- rescan.sh
+++ rescan.sh
@@ -1,2 +1,12 @@
+printf "sleep " 
 source ~/.profile
 ironfish wallet:rescan --reset
+
+printf "sleep 2" 
+
+        for((sec=0; sec<60; sec++))
+        do
+                printf "."
+                sleep 1
+        done
+        printf "\n"
